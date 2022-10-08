609f046cf92f08601f57435fd6f7293bed6f10c6
mcolaa <111545981+mcolaa@users.noreply.github.com>
2022-10-08 15:47:54 +0000

    Tmp

--- Sources/AppReview/AppReview.swift
+++ Sources/AppReview/AppReview.swift
@@ -77,3 +77,4 @@ public class AppReview {
             if #available(iOS 14.0, *) {
                 if let scene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
                     SKStoreReviewController.requestReview(in: scene)
+                }
